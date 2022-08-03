8fe3cf31db116b8becd3fde796ee9c8bd2195cce
Mofeng <363034937@qq.com>
2022-08-03 23:59:15 +0800

    Disabled React.StrickMode

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <HashRouter>
+    <App />
+  </HashRouter>
+  // </React.StrictMode>
+  ,
   document.getElementById('root')
 );
 
