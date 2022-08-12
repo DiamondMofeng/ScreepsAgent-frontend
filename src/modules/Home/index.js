d945d2bfa386e5d5edc685f7c9aa8462bd795f11
Mofeng <363034937@qq.com>
2022-08-13 05:18:45 +0800

    Perf: Home

--- src/modules/Home/index.js
+++ src/modules/Home/index.js
@@ -1,15 +1,15 @@
 const Home = () => {
   return (
     <div className='Home'>
-      <h1> Welcome to Mofeng's Screeps Agent</h1>
+      <h1> Welcome to screeps.mofengfeng.com !</h1>
       当前功能：
 
       <ul>
-        <li>图标代理</li>
+        <li>图表托管代理</li>
         <li>选房器(施工中)</li>
         <li>战斗力探测器(施工中)</li>
-        <li>
-          <ul>PublicAPI
+        <li>PublicAPI
+          <ul>
             <li>星门寻路</li>
           </ul>
         </li>
