41c2b848ed98dde649e7f8580030509d57ded3cf
Mofeng <363034937@qq.com>
2023-02-11 21:03:31 +0800

    fix: update cors url

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -2,7 +2,7 @@
 export const baseUrl = 'http://api-screeps.mofengfeng.com'
 // export const baseUrl= 'http://127.0.0.1:5000'
 
-export const corsUrl = 'https://mofeng-cors-anywhere.herokuapp.com'
+export const corsUrl = 'https://mofeng-cors-anywhere.onrender.com'
 
 export const roomImgUrlPrefix = 'https://d3os7yery2usni.cloudfront.net/map';//https://d3os7yery2usni.cloudfront.net/map/shard3/W19N14.png
 
